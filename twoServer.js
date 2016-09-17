7eeb22af1ef3b357f49d6defa0509efaecd4e50e
Kai <huikaizhong@yahoo.com>
2016-09-17 11:35:05 -0400

    update

--- twoServer.js
+++ twoServer.js
@@ -8,21 +8,21 @@ var bPORT=7500;
 //Create a server
 var aserver = http.createServer(function (request, response){
     // response.statusCode = 503;
-    response.end('server A Works!! Path Hit: ' + request.url);
+    response.end('It works! Path Hit: ' + request.url);
 });
 
 var bserver = http.createServer(function (request, response){
     // response.statusCode = 503;
-    response.end('server B Works!! Path Hit: ' + request.url);
+    response.end('It works! Path Hit: ' + request.url);
 });
 
 //Lets start our server
 aserver.listen(7000, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Good Words", aPORT);
+    console.log("server listening on: http://localhost:%s", aPORT);
 });
 
 bserver.listen(7500, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Bad Words", bPORT);
+    console.log("server listening on: http://localhost:%s", bPORT);
 });
